def4ccf6f38ba6f4a30956331f5cfca403e1375c
hxh1234 <anas.masaaf@gmail.com>
2015-12-18 23:54:36 +0000

    plan Amelioration

--- src/main/java/com/lambda/bilan/web/controllers/PlanAmeliorationController.java
+++ src/main/java/com/lambda/bilan/web/controllers/PlanAmeliorationController.java
@@ -4,14 +4,17 @@ import java.sql.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lambda.bilan.domain.PlanAmelioration;
 import com.lambda.bilan.entities.Collaborateur;
 import com.lambda.bilan.helpers.LambdaException;
 import com.lambda.bilan.metier.IPlanAmeliorationMetier;
 import com.lambda.bilan.web.helpers.ExceptionHelpers;
+import com.lambda.bilan.web.helpers.PropretiesHelper;
 import com.lambda.bilan.web.models.Reponse;
 
 @RestController
@@ -31,6 +34,20 @@ public class PlanAmeliorationController {
 		} catch (LambdaException e) {
 			return new Reponse(1,ExceptionHelpers.getErreursForException(e));
 		}
+	}
+	
+	/*
+	 * Ajouter un plan d'Amelioration pour un collaborateur
+	 */
+	
+	@RequestMapping(value = "/planAmeliorations", method = RequestMethod.POST, consumes = "application/json; charset=UTF-8")
+	public Reponse addColaborateur(@RequestBody PlanAmelioration planAmelioration) {
+		try {
+			planAmeliorationMetier.addPlanAmelioration(planAmelioration);
+		} catch (LambdaException e) {
+			return new Reponse(1,ExceptionHelpers.getErreursForException(e));
+		}
+		return new Reponse(0, PropretiesHelper.getText("planAmelioration.add.success"));
 	}
 
 }
--- src/main/java/com/lambda/bilan/web/controllers/UtilisateurController.java
+++ src/main/java/com/lambda/bilan/web/controllers/UtilisateurController.java
@@ -1,8 +1,5 @@
 package com.lambda.bilan.web.controllers;
 
-import java.sql.Date;
-import java.util.Calendar;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
